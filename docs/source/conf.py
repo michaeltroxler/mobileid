b6bba07f05a632f900e3b87fb482daf8bd602f74
michaeltroxler <37471624+michaeltroxler@users.noreply.github.com>
2025-11-12 11:54:20 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'Mobile ID'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2025, Swisscom'
+author = 'Swisscom'
 
 release = '0.1'
 version = '0.1.0'
